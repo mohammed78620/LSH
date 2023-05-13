dd8b01c3321bc6a646d978702dd414f765ecd980
Mohammed Akmal Miah <mohammedmiah67890@gmail.com>
2023-05-13 23:41:08 +0100

    fix folder

--- build.py
+++ build.py
@@ -5,8 +5,7 @@ from distutils.core import Distribution, Extension
 import numpy as np
 from Cython.Build import build_ext, cythonize
 
-cython_dir = os.path.join("lsh/compiled", "_ext")
-print(cython_dir)
+cython_dir = os.path.join("lsh", "_ext")
 extension = Extension(
     "lsh.cMinhash",
     [
@@ -17,9 +16,7 @@ extension = Extension(
     include_dirs=[np.get_include()],
     extra_compile_args=["-O3", "-std=c++17"],
 )
-print(extension)
 ext_modules = cythonize([extension], include_path=[cython_dir])
-print(ext_modules)
 dist = Distribution({"ext_modules": ext_modules})
 cmd = build_ext(dist)
 cmd.ensure_finalized()
@@ -27,4 +24,5 @@ cmd.run()
 
 for output in cmd.get_outputs():
     relative_extension = os.path.relpath(output, cmd.build_lib)
+    print(relative_extension)
     shutil.copyfile(output, relative_extension)

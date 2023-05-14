7730e2d4186095d9876ba45a61377c6db9a50859
Mohammed Akmal Miah <mohammedmiah67890@gmail.com>
2023-05-14 16:17:50 +0100

    fix build and toml

--- build.py
+++ build.py
@@ -5,32 +5,30 @@ from distutils.core import Distribution, Extension
 import numpy as np
 from Cython.Build import build_ext, cythonize
 
-cython_dir = os.path.join("lsh", "_ext")
-extension = Extension(
-    "lsh.cMinhash",
-    [
-        os.path.join(cython_dir, "cMinhash.pyx"),
-        os.path.join("lsh", "MurmurHash3.cpp"),
-        
-    ],
-    include_dirs=[np.get_include()],
-    extra_compile_args=["-O3", "-std=c++17"],
-)
-ext_modules = cythonize([extension], include_path=[cython_dir])
-dist = Distribution({"ext_modules": ext_modules})
-cmd = build_ext(dist)
-cmd.ensure_finalized()
-cmd.run()
 
-for output in cmd.get_outputs():
-    relative_extension = os.path.relpath(output, cmd.build_lib)
-    print(relative_extension)
-    shutil.copyfile(output, relative_extension)
+def build():
+    cython_dir = os.path.join("lsh", "_ext")
+    extension = Extension(
+        "lsh.cMinhash",
+        [
+            os.path.join(cython_dir, "cMinhash.pyx"),
+            os.path.join("lsh", "MurmurHash3.cpp"),
+            
+        ],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3", "-std=c++17"],
+    )
+    ext_modules = cythonize([extension], include_path=[cython_dir])
+    dist = Distribution({"ext_modules": ext_modules})
+    cmd = build_ext(dist)
+    cmd.ensure_finalized()
+    cmd.run()
 
-def build(setup_kwargs):
-    """Needed for the poetry building interface."""
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        print(relative_extension)
+        shutil.copyfile(output, relative_extension)
 
-    setup_kwargs.update({
-        'ext_modules' : ext_modules,
-        'include_dirs' : [np.get_include()],
-    })
+
+if __name__ == "__main__":
+    build()
